c0d3926c12498bc6afc4917625c24ae5677713b7
Sam Scott <samscott@Sams-MacBook-Air.local>
2025-06-03 11:45:26 +0100

    Initial commit

--- app.py
+++ app.py
@@ -173,5 +173,6 @@ def download(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
